474f7d915b54bc9a11e585e33c40f91402548f9c
LoganTann <logon313@hotmail.fr>
2021-04-18 15:40:39 +0200

    Ajout de la config des TU

---
+++ src/Test/ApplicationTest.java
@@ -0,0 +1,12 @@
+//import ;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+
+    @Test
+    public void main() {
+        // Utilisation de Junit 4. + harmcrest (inutilisé)
+        assertTrue(junit.runner.Version.id().startsWith("4."));
+    }
+}
