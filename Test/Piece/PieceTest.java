937cd41ebe9926835e27c6b0f8e864071cdf939f
LoganTann <logon313@hotmail.fr>
2021-05-04 19:59:37 +0200

    update des TU - Dissociation de Piece et Pawn en deux fichiers différents. En particulier, Piece contient un certain nombre de méthodes utilitaires pour réaliser des tâches de test difficiles - Suppression de PieceMoveRules.java dont les méthodes déplacées dans piece le seront de nouveau dans leurs classes respectives. - bug fix de références. - Ajout de javadoc

--- Test/Piece/PieceTest.java
+++ Test/Piece/PieceTest.java
@@ -2,10 +2,27 @@ package Piece;
 
 import Chessboard.Chessboard;
 import org.junit.Test;
+
+import static Chessboard.Chessboard.BOARD_SIZE;
+import static Piece.Piece.IS_WHITE;
 import static org.junit.Assert.*;
 
+import Chessboard.vect2D;
+
 public class PieceTest {
-    public static void testBadMoves(Chessboard chessboard, String[] moves, boolean[] isMoveValid) {
+
+    // METHODES STATIQUES UTILITAIRES ----------------------------------------------------------------------------------
+    // (utilisé à l'extérieur de cette classe)
+
+    /**
+     * Effectue une suite de mouvements (argument 2) et vérifie si celui-ci devrait (ou pas) throw une BadMoveException
+     * Si il n'y a pas correspondance, alors exécute fail(), qui va faire échouer le test appellant cette fonction.
+     * @param chessboard Le tableau de jeu contenant les pièces
+     * @param moves un tableau de String contenant une suite de mouvements à effectuer (ex: [a2a3, b2b4, c2c3])
+     * @param isMoveValid un tableau booléen de même taille que l'argument précédent. true si le mouvement associé
+     *                    est valide (ne devrait pas throw), false sinon.
+     */
+    protected static void testBadMoves(Chessboard chessboard, String[] moves, boolean[] isMoveValid) {
         assert moves.length == isMoveValid.length : "Erreur d'écriture de test, String.length != boolean.length";
         for (int i = 0; i < moves.length; i++) {
             String s = moves[i];
@@ -19,107 +36,135 @@ public class PieceTest {
                 fail( " Move " + s
                     + " is " + (isMoveValid[i] ? "valid": "not valid")
                     + " but play() " +  (didThrow ? "thrown BadMoveException": "didn't throw anything")
+                    + "\nChessboard state : \n" + chessboard.toString()
                 );
             }
         }
     }
 
-    @Test
-    public void testPlay_simpleStep() {
-        Chessboard chessboard = new Chessboard();
-        String moves[] = {
-            "a2a3", "a7a6", // Allez petit pas devant (tin tin)
-            "a3a2", "a3a4", "a6a7", "a6a5", // petit pas derrière (tin tin tin)... Allez petit pas devant (tin tin)
-            "a4a5", "h2h3", "a5a4", "h2h3"  // Toléka toléka -- coups pas valides car passage bloqué + idle moves
-        };
-        boolean validity[] = {true, true,    false, true, false, true,    false, true, false, true};
-        String expectedOut =
-                "    a   b   c   d   e   f   g   h    \n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "8 | t | c | f | r | d | f | c | t | 8\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "7 |   | p | p | p | p | p | p | p | 7\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "6 |   |   |   |   |   |   |   |   | 6\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "5 | p |   |   |   |   |   |   |   | 5\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "4 | P |   |   |   |   |   |   |   | 4\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "3 |   |   |   |   |   |   |   | P | 3\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "2 |   | P | P | P | P | P | P |   | 2\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "1 | T | C | F | R | D | F | C | T | 1\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "    a   b   c   d   e   f   g   h    \n";
+    /**
+     * Utilitaire graphique permettant de vérifier les règles de déplacement possibles d'une pièce
+     * Elle permet de tester {@link Piece#isValidMove(vect2D, vect2D)} par correspondance graphique (plus visuel dans
+     * l'écriture des tests, cf exemples plus bas). N'oublions pas que isValidMove ne teste que la validité du schéma de
+     * mouvement, et ne tiens PAS compte de l'état actuel du plateau (excepté les sorties de plateau)
+     * @param toTest la picèce à tester
+     * @param piecePosition la position de la pièce au sein du plateau
+     * @return Le résultat graphique généré.
+     */
+    protected static String graphical_moveDefsTester(Piece toTest, vect2D piecePosition) {
+        // Ne pas modifier ces constantes. Soit c'est ça, soit je devrai faire un code certes adaptable MAIS illisible
+        final char ME = '☺', EMPTY_WHITE = '□', EMPTY_BLACK='▤', NOT_EMPTY='▣', SPACE = ' ', LINE_SEPARATOR='\n';
 
-        testBadMoves(chessboard, moves, validity);
-        assertEquals(expectedOut, chessboard.toString());
-    }
-    @Test
-    public void testPlay_doubleStep() {
-        Chessboard chessboard = new Chessboard();
-        String moves[] = {
-            "a2a4", "b7b5", // normal (+2)
-            "a4a6", "a4a5", // recommence + barrage de route (+1)
-            "b5b3", "b5b4", // idem
-            "a5a6", "b4b3", // barrage de route en finition (+1)
-            "b2b4", "h2h3", // route barrée... + (idle move)
-            "a7a5", "h7h5"  //idem
-        };
-        boolean validity[] = {true, true,   false, true,   false, true,   true,true,   false, true,   false, true};
-        String expectedOut = "TODO a4a6 not valid"; // TODO : a4a6 not valid
-
-        testBadMoves(chessboard, moves, validity);
-        assertEquals(expectedOut, chessboard.toString());
-    }
-    @Test
-    public void testPlay_Eat() {
-        Chessboard chessboard = new Chessboard();
-        String moves[] = {
-            "a2b3", "a2a3", "a7b6", "a7a6", // manger du vide
-            "b2b4", "b7b5", // placement pour tenter de ...
-            "a3b4", "a3a4", "a6b5", "a6a5", // ... les rendre cannibale
-            "a4b5", "a5b4" // là ils peuvent manger
-        };
-        boolean validity[] = {false, true, false, true,   true, true,   false, true, false, true,    true, true};
-        String expectedOut = "    a   b   c   d   e   f   g   h    \n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "8 | t | c | f | r | d | f | c | t | 8\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "7 |   |   | p | p | p | p | p | p | 7\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "6 |   |   |   |   |   |   |   |   | 6\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "5 |   | P |   |   |   |   |   |   | 5\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "4 |   | p |   |   |   |   |   |   | 4\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "3 |   |   |   |   |   |   |   |   | 3\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "2 |   |   | P | P | P | P | P | P | 2\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "1 | T | C | F | R | D | F | C | T | 1\n" +
-                "   --- --- --- --- --- --- --- ---   \n" +
-                "    a   b   c   d   e   f   g   h    \n";
-
-        testBadMoves(chessboard, moves, validity);
-        assertEquals(expectedOut, chessboard.toString());
+        StringBuilder buffer = new StringBuilder();
+        for (int y = BOARD_SIZE - 1; y >= 0; y--) {
+            for (int x = 0; x < BOARD_SIZE; x++) {
+                if (piecePosition.x == x && piecePosition.y == y) {
+                    buffer.append(ME);
+                } else if ( toTest.isValidMove(piecePosition, new vect2D(x, y)) ) {
+                    buffer.append(NOT_EMPTY);
+                } else if (y % 2 == x % 2) {
+                    buffer.append(EMPTY_BLACK);
+                } else {
+                    buffer.append(EMPTY_WHITE);
+                }
+                buffer.append(SPACE);
+            }
+            buffer.append(LINE_SEPARATOR);
+        }
+        return buffer.toString();
     }
 
-    public void testTestClone() {
-        Piece original = new Pawn(true);
-        Piece hisClone = original.clone();
-        assertEquals(original,hisClone); // Contenu identique
-        assertNotSame(original,hisClone); // ... sauf la référence
-        assertTrue(hisClone instanceof Pawn); // et doit être un Pawn, pas un pion non spécifié
+
+    // TEST UNITAIRE DE LA CLASSE PIECE --------------------------------------------------------------------------------
+
+    /**
+     * Cette classe teste visuellement les mouvements horizontaux ou verticaux, ie. {@link Tower#isLinearMove}
+     * @see Tower#isValidMove(vect2D, vect2D)
+     */
+    @Test
+    public void graphical_towerMove() {
+        String expected =
+                "□ ▤ □ ▣ □ ▤ □ ▤ \n" +
+                        "▤ □ ▤ ▣ ▤ □ ▤ □ \n" +
+                        "□ ▤ □ ▣ □ ▤ □ ▤ \n" +
+                        "▤ □ ▤ ▣ ▤ □ ▤ □ \n" +
+                        "▣ ▣ ▣ ☺ ▣ ▣ ▣ ▣ \n" +
+                        "▤ □ ▤ ▣ ▤ □ ▤ □ \n" +
+                        "□ ▤ □ ▣ □ ▤ □ ▤ \n" +
+                        "▤ □ ▤ ▣ ▤ □ ▤ □ \n";
+        String out = graphical_moveDefsTester(new Tower(IS_WHITE), vect2D.createFromChessCoord("d4"));
+        assertEquals(expected, out);
     }
 
     /**
-     * @see PiecesMoveRules#graphical_PawnMove()
+     * Cette classe teste visuellement les mouvements en diagonale, ie. {@link Bishop#isDiagMove}
+     * @see Bishop#isValidMove(vect2D, vect2D)
      */
-    public void testIsValidMove() {
-        // test implémenté ici : PiecesMoveRules.graphical_PawnMove()
+    @Test
+    public void graphical_bishopMove() {
+        String expected_w =
+                "□ ▤ □ ▤ □ ▤ □ ▣ \n" +
+                        "▣ □ ▤ □ ▤ □ ▣ □ \n" +
+                        "□ ▣ □ ▤ □ ▣ □ ▤ \n" +
+                        "▤ □ ▣ □ ▣ □ ▤ □ \n" +
+                        "□ ▤ □ ☺ □ ▤ □ ▤ \n" +
+                        "▤ □ ▣ □ ▣ □ ▤ □ \n" +
+                        "□ ▣ □ ▤ □ ▣ □ ▤ \n" +
+                        "▣ □ ▤ □ ▤ □ ▣ □ \n";
+        String expected_b =
+                "▣ ▤ □ ▤ □ ▤ ▣ ▤ \n" +
+                        "▤ ▣ ▤ □ ▤ ▣ ▤ □ \n" +
+                        "□ ▤ ▣ ▤ ▣ ▤ □ ▤ \n" +
+                        "▤ □ ▤ ☺ ▤ □ ▤ □ \n" +
+                        "□ ▤ ▣ ▤ ▣ ▤ □ ▤ \n" +
+                        "▤ ▣ ▤ □ ▤ ▣ ▤ □ \n" +
+                        "▣ ▤ □ ▤ □ ▤ ▣ ▤ \n" +
+                        "▤ □ ▤ □ ▤ □ ▤ ▣ \n";
+        String out_w = graphical_moveDefsTester(new Bishop(IS_WHITE), vect2D.createFromChessCoord("d4"));
+        String out_b = graphical_moveDefsTester(new Bishop(! IS_WHITE), vect2D.createFromChessCoord("d5"));
+        assertEquals(expected_w, out_w);
+        assertEquals(expected_b, out_b);
+    }
+    @Test
+    public void graphical_knightMove() {
+        String expected =
+                "□ ▤ □ ▤ □ ▤ □ ▤ \n" +
+                        "▤ □ ▤ □ ▤ □ ▤ □ \n" +
+                        "□ ▤ ▣ ▤ ▣ ▤ □ ▤ \n" +
+                        "▤ ▣ ▤ □ ▤ ▣ ▤ □ \n" +
+                        "□ ▤ □ ☺ □ ▤ □ ▤ \n" +
+                        "▤ ▣ ▤ □ ▤ ▣ ▤ □ \n" +
+                        "□ ▤ ▣ ▤ ▣ ▤ □ ▤ \n" +
+                        "▤ □ ▤ □ ▤ □ ▤ □ \n";
+        String out = graphical_moveDefsTester(new Knight(IS_WHITE), vect2D.createFromChessCoord("d4"));
+        assertEquals(expected, out);
+    }
+    @Test
+    public void graphical_queenMove() {
+        String expected =
+                "□ ▤ □ ▣ □ ▤ □ ▣ \n" +
+                        "▣ □ ▤ ▣ ▤ □ ▣ □ \n" +
+                        "□ ▣ □ ▣ □ ▣ □ ▤ \n" +
+                        "▤ □ ▣ ▣ ▣ □ ▤ □ \n" +
+                        "▣ ▣ ▣ ☺ ▣ ▣ ▣ ▣ \n" +
+                        "▤ □ ▣ ▣ ▣ □ ▤ □ \n" +
+                        "□ ▣ □ ▣ □ ▣ □ ▤ \n" +
+                        "▣ □ ▤ ▣ ▤ □ ▣ □ \n";
+        String out = graphical_moveDefsTester(new Queen(IS_WHITE), vect2D.createFromChessCoord("d4"));
+        assertEquals(expected, out);
+    }
+    @Test
+    public void graphical_KingMove() {
+        String expected =
+                "□ ▤ □ ▤ □ ▤ □ ▤ \n" +
+                        "▤ □ ▤ □ ▤ □ ▤ □ \n" +
+                        "□ ▤ □ ▤ □ ▤ □ ▤ \n" +
+                        "▤ □ ▣ ▣ ▣ □ ▤ □ \n" +
+                        "□ ▤ ▣ ☺ ▣ ▤ □ ▤ \n" +
+                        "▤ □ ▣ ▣ ▣ □ ▤ □ \n" +
+                        "□ ▤ □ ▤ □ ▤ □ ▤ \n" +
+                        "▤ □ ▤ □ ▤ □ ▤ □ \n";
+        String out = graphical_moveDefsTester(new King(IS_WHITE), vect2D.createFromChessCoord("d4"));
+        assertEquals(expected, out);
     }
 }
